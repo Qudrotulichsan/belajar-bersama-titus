8dd14b101e2218c09c2051c9fb5d46d61ca04e48
Qudrotulichsan <qudrotulichsan@gmail.com>
2023-03-25 22:26:58 +0700

    2nd commit

--- exercise.js
+++ exercise.js
@@ -266,3 +266,40 @@
 // var fullSentence = processSentence(name,age,address,hobby);
 // console.log(fullSentence);
 
+
+
+// exercise 10
+// function bandingkanAngka(angka1, angka2) {
+//     if(angka1 < angka2){
+//         console.log("true");
+//     } else if(angka1 > angka2 ) {
+//         console.log('false')
+//     } else {
+//         console.log ('-1')
+//     }
+//   }
+  
+//   // TEST CASES
+//   console.log(bandingkanAngka(5, 8)); // true
+//   console.log(bandingkanAngka(5, 3)); // false
+//   console.log(bandingkanAngka(4, 4)); // -1
+//   console.log(bandingkanAngka(3, 3)); // -1
+//   console.log(bandingkanAngka(17, 2)); // false
+  
+
+// exercise 11
+
+function balikKata(kata) {
+
+    var kalimat = "hello ican"
+    for(let i=0; i > kalimat.length; i--){
+        console.log(kalimat)
+    }
+}
+  
+  // TEST CASES
+  console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+//   console.log(balikKata('John Doe')); // eoD nhoJ
+//   console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+//   console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+//   console.log(balikKata('Super')); // repuS
